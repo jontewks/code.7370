e16b90887e794d6301166e14b5bb6f11ab7a06c9
Jon Tewksbury <jontewks@gmail.com>
2015-08-24 21:12:48 -0700

    Add and start on next section

---
+++ 09 - Functional JavaScript/1 - Basic Function Exercises/jon_exercise_01/function-invocation.js
@@ -0,0 +1,40 @@
+// Below we have the name of a function that we are going to pretend exists:
+// addNumbersTogether
+
+// But if we ran the code as it is if it were uncommented, it wouldn't work right. That's
+// because we haven't invoked the function yet, or told the computer to run the function's
+// code. What change do we need to make to the function in order to invoke it? Write the
+// new code with that change below:
+
+
+// Now that we have invoked the function, it appears that we need to pass the function
+// some arguments. Arguments are what we give a funciton in order for it to work correctly.
+// Lets pretend that we need to give the function two arguments, both numbers, for it to add.
+// On the lines below, write what that might look like:
+
+
+// Now lets pretend it can take as many arguments as we want. On the lines below, write
+// what it would look like if we were to give the function 4 numbers to add together.
+
+
+// Above, we were passing numbers in to a function as arguments, but did you know that a
+// function can take any type of object as an argument? This means we can pass strings,
+// numbers, floats, arrays, objects, variables, or other functions as arguments.
+// On the lines below, modify the function call so that we pass in a string as the first
+// and only argument.
+
+// thisFunctionExpectsAString()
+
+
+// Now lets try it with a few different types of objects:
+
+// thisFunctionExpectsAnArray()
+// thisFunctionExpectsAnObject()
+// thisFunctionExpectsAVariable()
+
+
+// Now that you see you can pass any type of object into a function as an argument, lets
+// combine that ability with the ability to pass in multiple arguments into the same function.
+// The function below is expecting two arrays as arguments, lets give them to it!
+
+// combineArrays()
