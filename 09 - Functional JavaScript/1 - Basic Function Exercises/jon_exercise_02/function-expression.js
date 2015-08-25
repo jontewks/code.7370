d09857d1d805790b3f6f1bae7dfc2357a3cf71c3
Jon Tewksbury <jontewks@gmail.com>
2015-08-24 23:02:14 -0700

    Add function expressions

---
+++ 09 - Functional JavaScript/1 - Basic Function Exercises/jon_exercise_02/function-expression.js
@@ -0,0 +1,29 @@
+// There are a few ways to create functions in JavaScript. The first way we will be
+// going over is called function expression. A function expression is simply creating
+// a variable, just like we have in the past, but instead of assigning a value to it,
+// we assign a function to it. Take a look at the following example:
+
+// var sayHello = function () {
+//   console.log('Hello!');
+// };
+
+// In that example, we are creating the variable "sayHello" and then assigning a function
+// to the variable so that if someone were to write the following code:
+// sayHello();
+// Then our function would run, and would console.log "Hello!".
+
+// On the lines below, use function expression to create a function that can do whatever
+// you would like it to.
+
+
+// Now that you have some experience with function expressions, lets write another function
+// expression, but this function should take some arguments! Here is a quick example for
+// reference later on:
+
+// var sayHelloTo = function (name) {
+//   console.log('Hello', name);
+// };
+
+// Inside those parentheses is where we can specify how many arguments our function
+// expects. On the lines below, write a function expression that expects to take more
+// than one argument.
